e6a778ab9511f0997536e6d24bd3b567856b1a91
borg <bruno.riguetto@gft.com>
2022-11-04 19:58:57 -0300

    Service layer, component registration

--- src/main/java/com/gft/courseNelio/resources/UserController.java
+++ src/main/java/com/gft/courseNelio/resources/UserController.java
@@ -1,19 +1,39 @@
 package com.gft.courseNelio.resources;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gft.courseNelio.entities.User;
+import com.gft.courseNelio.services.UserService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(value = "api/v1/users")
+@AllArgsConstructor
 public class UserController {
 
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		return null;
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
---
+++ src/main/java/com/gft/courseNelio/services/UserService.java
@@ -0,0 +1,29 @@
+package com.gft.courseNelio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.gft.courseNelio.entities.User;
+import com.gft.courseNelio.repositories.UserRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+}
