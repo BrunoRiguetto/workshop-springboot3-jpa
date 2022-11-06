63421749c410881668f7093c39be9540628971e9
borg <bruno.riguetto@gft.com>
2022-11-06 16:45:08 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/gft/courseNelio/entities/User.java
+++ src/main/java/com/gft/courseNelio/entities/User.java
@@ -1,11 +1,14 @@
 package com.gft.courseNelio.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,7 +17,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -26,5 +28,18 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+	
+	
 	
 }
