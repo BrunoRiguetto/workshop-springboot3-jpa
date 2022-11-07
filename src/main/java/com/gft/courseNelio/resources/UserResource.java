e0d6342977d47e16cc6d9adbb1c4c834453ae4c1
borg <bruno.riguetto@gft.com>
2022-11-07 18:43:40 -0300

    User insert

--- src/main/java/com/gft/courseNelio/resources/UserResource.java
+++ src/main/java/com/gft/courseNelio/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.gft.courseNelio.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gft.courseNelio.entities.User;
 import com.gft.courseNelio.services.UserService;
@@ -16,7 +20,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping(value = "api/v1/users")
 @AllArgsConstructor
-public class UserController {
+public class UserResource {
 
 	private UserService service;
 	
@@ -34,6 +38,13 @@ public class UserController {
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);		
 	}
 	
 }
