b42a42680015c20969556dd6135194d4bca70f2b
borg <bruno.riguetto@gft.com>
2022-11-07 19:42:01 -0300

    Exception handling - delete

--- src/main/java/com/gft/courseNelio/services/UserService.java
+++ src/main/java/com/gft/courseNelio/services/UserService.java
@@ -3,10 +3,13 @@ package com.gft.courseNelio.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gft.courseNelio.entities.User;
 import com.gft.courseNelio.repositories.UserRepository;
+import com.gft.courseNelio.services.exceptions.DatabaseException;
 import com.gft.courseNelio.services.exceptions.ResourceNotFoundException;
 
 import lombok.AllArgsConstructor;
@@ -33,7 +36,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
